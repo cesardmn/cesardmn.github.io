85df59293e794606b53be8411bfcd8bf9f15e231
cesardmn <cesardmn@gmail.com>
2023-01-25 10:37:50 -0300

    add new files

---
+++ assets/js/index.js
@@ -0,0 +1,5 @@
+(function Copyright() {
+  const date = new Date().getFullYear();
+  const footer = document.querySelector("footer");
+  footer.innerText = `Copyright © Cesar Dimi - ${date}`;
+})();
