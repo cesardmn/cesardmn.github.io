9e805a9ae827f95aa57f14b1395c2aff9e317cf3
cesardmn <cesardmn@gmail.com>
2025-06-18 23:20:54 -0300

    :core: Configurações iniciais para React + Vite + Tailwindcss.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/img/cesar_dimi_full_stack_developer.webp'
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+    </>
+  )
+}
+
+export default App
