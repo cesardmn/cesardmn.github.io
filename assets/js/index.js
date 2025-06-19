cd720bf0cf0ded96e2af7e4b37fa9a20cbf40217
cesardmn <cesardmn@gmail.com>
2025-06-18 22:40:48 -0300

    core: Deletando arquivos inutilizados.

--- assets/js/index.js
+++
@@ -1,5 +0,0 @@
-(function Copyright() {
-  const date = new Date().getFullYear();
-  const footer = document.querySelector("footer");
-  footer.innerText = `Copyright © Cesar Dimi - ${date}`;
-})();
